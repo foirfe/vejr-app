8a93f795484d2888f4970da3c9542a14a9cfa36e
Daniel Freyr <dannalingur@hotmail.com>
2025-11-05 13:24:44 +0100

    Index Structure - JS Init

---
+++ global.js
@@ -0,0 +1,8 @@
+//GLOBAL
+var apiUrl = `https://api.weatherapi.com/v1/forecast.json`
+var apiKey = "9dee96c4230c4dd0b1180923250511"
+
+/*
+EKSEMPEL på fetch kald
+fetch(`${apiUrl}?q="London"&days=5&key=${apiKey}`)
+ */
